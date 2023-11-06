7f0078f580709c4bfd2ac4fdecbbecadfb31fed8
Richard Perales <isc.rperales@gmail.com>
2023-11-06 11:41:42 -0600

    ajustes generales
    Se actualiza GIT con última versión funcional y se agrega documento de solicitud de cambios de Adolfo e imágenes y videos que desea publicar en la sección galería

--- mabapesh/ajaxmail.php
+++ mabapesh/ajaxmail.php
@@ -3,28 +3,29 @@ $un=$_POST['username'];
 $em=$_POST['useremail'];
 $su=$_POST['useresubject'];
 $msg=$_POST['mesg'];
+$correoequipobamapesh = 'davidhsc@gmail.com';
 if(trim($un)!="" && trim($msg)!="" && trim($su)!="" && trim($em)!="")
 {
 	if(filter_var($em, FILTER_VALIDATE_EMAIL))
 	{
-		$message="Hi Admin..<p>".$un." has sent a query having subject".$su."email id as ".$em."</p><p>Query is : ".$msg."</p>";
+		$message="Estimado equipo Mabapesh<p>".$un."(".$em.") ha enviado un mensaje desde el formulario de contacto. "."</p><p>Asunto:".$su."<br>mensaje : ".$msg."</p>";
 		$headers = "MIME-Version: 1.0" . "\r\n";
 		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 		$headers .= 'From: <support@transport.com>' . "\r\n";
 
-		if(mail('jhini.mehta@gmail.com','Query for transport',$message,$headers ))
+		if(mail($correoequipobamapesh,'Mensaje desde formulario de contacto',$message,$headers ))
 		{
-		echo '1#<p style="color:green;">Mail has been sent successfully.</p>';
+		echo '1#<p style="color:green;">Correo enviado exitosamente. En breve nos ponemos en contacto.</p>';
 		}
 		else
 		{
-		echo '2#<p style="color:red;">Please, Try Again.</p>';
+		echo '2#<p style="color:red;">Por favor intente nuevamente.</p>';
 		}
 	}
 	else
-		echo '2#<p style="color:red">Please, provide valid Email.</p>';
+		echo '2#<p style="color:red">Por favor proporcione un correo válido.</p>';
 }
 else
 {
-echo '2#<p style="color:red">Please, fill all the details.</p>';
+echo '2#<p style="color:red">Por favor proporcione todos los datos.</p>';
 }?>
